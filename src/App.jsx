6085953687a1970dd717cdb94614d47c78df793b
achangofpace <55330719+achangofpace@users.noreply.github.com>
2024-07-09 18:35:01 -0700

    learn styled components a bit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={()=>alert("Check in")}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={()=>alert("Check out")}>Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
