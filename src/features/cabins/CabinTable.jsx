ecca6f6ed2b9864f7430be665687aa9ae264a224
achangofpace <55330719+achangofpace@users.noreply.github.com>
2024-07-15 15:00:29 -0700

    add sorting and filtering to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
 
 import { useCabins } from "./useCabins";
 
@@ -7,27 +7,29 @@ import Menus from "../../ui/Menus";
 import Table from "../../ui/Table";
 import CabinRow from "./CabinRow";
 
-// const TableHeader = styled.header`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-
-//   background-color: var(--color-grey-50);
-//   border-bottom: 1px solid var(--color-grey-100);
-//   text-transform: uppercase;
-//   letter-spacing: 0.4px;
-//   font-weight: 600;
-//   color: var(--color-grey-600);
-//   padding: 1.6rem 2.4rem;
-// `;
-
 function CabinTable() {
   const { isPending, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isPending) {
     return <Spinner />;
   }
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") {
+    filteredCabins = cabins;
+  } else if (filterValue === "with-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  } else if (filterValue === "no-discount") {
+    filteredCabins = cabins.filter((cabin) => !cabin.discount);
+  }
+
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort((a,b) => (a[field] - b[field]) * modifier);
 
   return (
     <Menus>
@@ -41,7 +43,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(cabin) => (
             <CabinRow key={cabin.id} cabin={cabin}></CabinRow>
           )}
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,7 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
@@ -10,6 +11,7 @@ function Cabins() {
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
         <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row>
